6da5d9dd3ffad85567185942dfcfc879b95cac3f
Mariela Portillo <ruthmarielaorellana1998@gmail.com>
2021-10-14 19:34:30 -0600

    Inicar Proy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CineAppComponent } from './models/cine-app/cine-app.component';
+import { ComercioComponent } from './models/comercio/comercio.component';
+import { RedSocialComponent } from './models/red-social/red-social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CineAppComponent,
+    ComercioComponent,
+    RedSocialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/cine-app/cine-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cine-app',
+  templateUrl: './cine-app.component.html',
+  styleUrls: ['./cine-app.component.css']
+})
+export class CineAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
