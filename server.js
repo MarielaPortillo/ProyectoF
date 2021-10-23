2684b3f2bff261e512d2366d58cce9716b15fab0
Mariela Portillo <ruthmarielaorellana1998@gmail.com>
2021-10-22 18:07:58 -0600

    Deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// Serve static files
+
 app.use(express.static(__dirname + '/dist/ProyectoF'));
 
 // Send all requests to index.html
